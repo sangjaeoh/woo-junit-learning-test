722b9c073654398d3ffbf6ea2cbbb768ac7902dc
sangjaeoh <sangjaeoh91@gmail.com>
2022-09-28 22:34:34 +0900

    first

---
+++ src/main/java/com/learning/WooJunitLearningTestApplication.java
@@ -0,0 +1,13 @@
+package com.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WooJunitLearningTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WooJunitLearningTestApplication.class, args);
+    }
+
+}
